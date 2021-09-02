3e615c2e8ae2c237947dee203515480a3d19ae52
Joseph765 <joseph.chauvin102@gmail.com>
2021-09-01 20:04:31 -0500

    fixed error in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
-NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-});
-
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
 
 export class AppRoutingModule { }
